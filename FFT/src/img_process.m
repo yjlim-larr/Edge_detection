353036ab09f4dd571b9c2834c6d2731f0d3a86f8
lim-yong-jae <62493307+lim-yong-jae@users.noreply.github.com>
2021-12-28 09:31:13 +0900

    Matlab code

--- FFT/src/img_process.m
+++ FFT/src/img_process.m
@@ -1,3 +1,4 @@
+% Matlab code
 clear all 
 close all
 clc
@@ -12,7 +13,7 @@ h = arr(1)
 w = arr(2)
 
 
-%plot3방법
+% plot3방법
 X = 0:1:w-1
 Y = 0:1:h-1
 h1 = surf(X * 2 *pi / w, Y * 2 * pi/ h, abs(F_im / (h*w)), 'edgecolor', 'none')
@@ -20,12 +21,12 @@ xlim([-1, 10])
 ylim([-1,10])
 set(h1,'LineStyle','none')
 
-%img_recover without any processed
+% img_recover without any processed
 inverse_im = uint8(abs(ifft2(F_im)))
 f = figure
 imshow(inverse_im)
 
-%plot spectrum
+% plot spectrum
 mag = abs(F_im)
 phase = angle(F_im)
 spectrum = log(fftshift(mag))
@@ -40,7 +41,7 @@ i = h/2-10:1:h/2+10
 j = w/2-20:1:w/2+20
 F_im3(i, j) = shift_F_im(i, j)
 
-%image_recover with magnitude 0 on high frequency's magnitude.
+% image_recover with magnitude 0 on high frequency's magnitude.
 inverse_im2 = uint8(abs(ifft2(F_im3)))
 f = figure
 imshow(inverse_im2)
@@ -48,17 +49,17 @@ imshow(inverse_im2)
 % insert 0 to high frequency's magnitude.
 shift_F_im = shift_F_im - F_im3
 
-%image_recover with magnitude 0 on high frequency's magnitude.
+% image_recover with magnitude 0 on high frequency's magnitude.
 inverse_im3 = uint8(abs(ifft2(shift_F_im)))
 f = figure
 imshow(inverse_im3)
 
-% differentiator
+% Differentiator
 d_im = diff(pre_im)
 f1 = figure
 imshow(d_im)
 
-%Differentiated image's spectrum
+% Differentiated image's spectrum
 F_d_im = fft2(d_im)
 
 X = 0:1:w-1
